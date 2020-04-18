3b633f35201277e72134fc171463f7d346a16397
Shaharyaar7 <55107159+Shaharyaar7@users.noreply.github.com>
2020-04-19 02:37:21 +0530

    Update login.php

--- login.php
+++ login.php
@@ -123,7 +123,7 @@ body {
     left: 0;
     width: 100%;
     height: 125%; /* To compensate for mobile browser address bar space */
-    background: url(./images/bg.png) no-repeat; 
+    background: url(bg.png) no-repeat; 
     background-size: 100% 100%;
 }
 
