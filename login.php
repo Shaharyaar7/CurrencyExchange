ac6e9f2d9bdd878fb292009cd23267edd1b0f147
Shaharyaar7 <55107159+Shaharyaar7@users.noreply.github.com>
2020-04-19 11:46:54 +0530

    Update login.php

--- login.php
+++ login.php
@@ -200,7 +200,7 @@ input[type=password] {
                             <div class="form-group">
                                 <input type="submit" class="btn btn-primary" value="Login">
                             </div>
-                            <p  style="color:#6E6E6E;">Don't have an account? <a href="register.php" style="color: black;">Sign up now</a>.</p>
+                            <p  style="color:white;">Don't have an account? <a href="register.php" style="color: black;">Sign up now</a>.</p>
                         </form>
                     </div>
             </div>  
